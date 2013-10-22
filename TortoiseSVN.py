043d7b4635b12f4ed656f9734b6ca4aa8ded5e03
lite3 <lite3@qq.com>
2013-10-22 14:41:54 +0800

    add svn revert

--- TortoiseSVN.py
+++ TortoiseSVN.py
@@ -51,6 +51,30 @@ class SvnCommitCommand(sublime_plugin.TextCommand):
 		_svn_command('commit', dir)
 
 
+class SvnRevertCommand(sublime_plugin.TextCommand):
+
+	def run(self, edit, paths=None):
+		if paths:
+			dir = '*'.join(paths)
+		else:
+			dir = self.view.file_name()
+
+		_svn_command('revert', dir)
+
+		(row,col) = self.view.rowcol(self.view.sel()[0].begin())
+		self.lastLine = str(row + 1);
+
+		if not paths:
+			sublime.set_timeout(self.revert, 100)
+
+	def revert(self):
+		self.view.run_command('revert')
+		sublime.set_timeout(self.revertPoint, 600)
+
+	def revertPoint(self):
+		self.view.window().run_command('goto_line',{'line':self.lastLine})
+
+
 class SvnLogCommand(sublime_plugin.TextCommand):
 
 	def run(self, edit, paths=None):
