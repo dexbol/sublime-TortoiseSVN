1f2bb511cf005d8c67d015dc6eeffec470622b14
dexbol <dexbolg@gmail.com>
2013-11-05 18:36:16 +0800

    remove tab

--- TortoiseSVN.py
+++ TortoiseSVN.py
@@ -21,10 +21,10 @@ class TortoiseSvnCommand(sublime_plugin.WindowCommand):
 		proce = subprocess.Popen('"' + tortoiseproc_path + '"' + ' /closeonend:3' + 
 			' /command:' + cmd + ' /path:"%s"' % dir , stdout=subprocess.PIPE)
 
-  		# This is required, cause of ST must wait TortoiseSVN update then revert
-        # the file. Otherwise the file reverting occur before SVN update, if the
-        # file changed the file content in ST is older.
-        proce.communicate()
+		# This is required, cause of ST must wait TortoiseSVN update then revert
+		# the file. Otherwise the file reverting occur before SVN update, if the
+		# file changed the file content in ST is older.
+		proce.communicate()
 
 
 class MutatingTortoiseSvnCommand(TortoiseSvnCommand):
