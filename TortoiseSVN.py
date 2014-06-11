00e1557b81c79e4735bd7ffc7f0654ea69241a55
dexbol <dexbolg@gmail.com>
2014-06-11 14:48:09 +0800

    1) replace tab to spaces. 2) add 'Add' command . 3) add config for auto close commit dialog

--- TortoiseSVN.py
+++ TortoiseSVN.py
@@ -5,98 +5,109 @@ import os.path
 import subprocess
 
 class TortoiseSvnCommand(sublime_plugin.WindowCommand):
-	def run(self, cmd, paths=None, isHung=False):
-		dir = self.getPath(paths)
+    def run(self, cmd, paths=None, isHung=False):
+        dir = self.get_path(paths)
 
-		if not dir:
-			return
+        if not dir:
+            return
+            
+        settings = self.get_setting()
+        tortoiseproc_path = settings.get('tortoiseproc_path')
 
-		settings = sublime.load_settings('TortoiseSVN.sublime-settings')
-		tortoiseproc_path = settings.get('tortoiseproc_path')
+        if not os.path.isfile(tortoiseproc_path):
+            sublime.error_message('can\'t find TortoiseProc.exe,'
+            	' please config setting file' '\n   --sublime-TortoiseSVN')
+            raise
 
-		if not os.path.isfile(tortoiseproc_path):
-			sublime.error_message(''.join(['can\'t find TortoiseProc.exe,',
-				' please config setting file', '\n   --sublime-TortoiseSVN']))
-			raise
+        proce = subprocess.Popen('"' + tortoiseproc_path + '"' + 
+            ' /command:' + cmd + ' /path:"%s"' % dir , stdout=subprocess.PIPE)
 
-		proce = subprocess.Popen('"' + tortoiseproc_path + '"' +
-			' /command:' + cmd + ' /path:"%s"' % dir , stdout=subprocess.PIPE)
+        # This is required, cause of ST must wait TortoiseSVN update then revert
+        # the file. Otherwise the file reverting occur before SVN update, if the
+        # file changed the file content in ST is older.
+        if isHung:
+            proce.communicate()
 
-		# This is required, cause of ST must wait TortoiseSVN update then revert
-		# the file. Otherwise the file reverting occur before SVN update, if the
-		# file changed the file content in ST is older.
-		if isHung:
-			proce.communicate()
+    def get_path(self, paths):
+        path = None
+        if paths:
+            path = '*'.join(paths)
+        else:
+            view = sublime.active_window().active_view()
+            path = view.file_name() if view else None
 
-	def getPath(self, paths):
-		path = None
-		if paths:
-			path = '*'.join(paths)
-		else:
-			view = sublime.active_window().active_view()
-			path = view.file_name() if view else None
+        return path
 
-		return path
+    def get_setting(self):
+        return sublime.load_settings('TortoiseSVN.sublime-settings')
 
 
 class MutatingTortoiseSvnCommand(TortoiseSvnCommand):
-	def run(self, cmd, paths=None):
-		TortoiseSvnCommand.run(self, cmd, paths, True)
+    def run(self, cmd, paths=None):
+        TortoiseSvnCommand.run(self, cmd, paths, True)
+        
+        self.view = sublime.active_window().active_view()
+        row, col = self.view.rowcol(self.view.sel()[0].begin())
+        self.lastLine = str(row + 1);
+        sublime.set_timeout(self.revert, 100)
 
-		self.view = sublime.active_window().active_view()
-		row, col = self.view.rowcol(self.view.sel()[0].begin())
-		self.lastLine = str(row + 1);
-		sublime.set_timeout(self.revert, 100)
+    def revert(self):
+        self.view.run_command('revert')
+        sublime.set_timeout(self.revertPoint, 600)
 
-	def revert(self):
-		self.view.run_command('revert')
-		sublime.set_timeout(self.revertPoint, 600)
-
-	def revertPoint(self):
-		self.view.window().run_command('goto_line',{'line':self.lastLine})
+    def revertPoint(self):
+        self.view.window().run_command('goto_line', {'line':self.lastLine})
 
 
 class SvnUpdateCommand(MutatingTortoiseSvnCommand):
-	def run(self, paths=None):
-		settings = sublime.load_settings('TortoiseSVN.sublime-settings')
-		closeonend = ('3' if True == settings.get('autoCloseUpdateDialog')
-			else '0')
-		MutatingTortoiseSvnCommand.run(self, 'update /closeonend:' + closeonend,
-			paths)
+    def run(self, paths=None):
+        settings = self.get_setting()
+        closeonend = ('3' if True == settings.get('autoCloseUpdateDialog')
+            else '0')
+        MutatingTortoiseSvnCommand.run(self, 'update /closeonend:' + closeonend, 
+            paths)
 
 
 class SvnCommitCommand(TortoiseSvnCommand):
-	def run(self, paths=None):
-		TortoiseSvnCommand.run(self, 'commit /closeonend:3', paths)
+    def run(self, paths=None):
+        settings = self.get_setting()
+        closeonend = ('3' if True == settings.get('autoCloseCommitDialog')
+            else '0')
+        TortoiseSvnCommand.run(self, 'commit /closeonend:' + closeonend, paths)
 
 
 class SvnRevertCommand(MutatingTortoiseSvnCommand):
-	def run(self, paths=None):
-		MutatingTortoiseSvnCommand.run(self, 'revert', paths)
+    def run(self, paths=None):
+        MutatingTortoiseSvnCommand.run(self, 'revert', paths)
 
 
 class SvnLogCommand(TortoiseSvnCommand):
-	def run(self, paths=None):
-		TortoiseSvnCommand.run(self, 'log', paths)
+    def run(self, paths=None):
+        TortoiseSvnCommand.run(self, 'log', paths)
 
 
 class SvnSwitchCommand(TortoiseSvnCommand):
-	def run(self, paths=None):
-		TortoiseSvnCommand.run(self, 'switch', paths)
+    def run(self, paths=None):
+        TortoiseSvnCommand.run(self, 'switch', paths)
 
 
 class SvnDiffCommand(TortoiseSvnCommand):
-	def run(self, paths=None):
-		TortoiseSvnCommand.run(self, 'diff', paths)
+    def run(self, paths=None):
+        TortoiseSvnCommand.run(self, 'diff', paths)
 
 
 class SvnBlameCommand(TortoiseSvnCommand):
-	def run(self, paths=None):
-		view = sublime.active_window().active_view()
-		row = view.rowcol(view.sel()[0].begin())[0] + 1
+    def run(self, paths=None):
+        view = sublime.active_window().active_view()
+        row = view.rowcol(view.sel()[0].begin())[0] + 1
 
-		TortoiseSvnCommand.run(self, 'blame /line:' + str(row), paths)
+        TortoiseSvnCommand.run(self, 'blame /line:' + str(row), paths)
 
-	def is_visible(self, paths=None):
-		file = self.getPath(paths)
-		return os.path.isfile(file) if file else False
+    def is_visible(self, paths=None):
+        file = self.get_path(paths)
+        return os.path.isfile(file) if file else False
+
+
+class SvnAddCommand(TortoiseSvnCommand):
+    def run(self, paths=None):
+        TortoiseSvnCommand.run(self, 'add', paths)
